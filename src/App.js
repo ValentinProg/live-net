c0c3e2e697ae339770688e9c1f1961a744519fe5
Valentin <V.Kovalenko.prog@gmail.com>
2022-08-29 12:21:21 +0300

    add button component with styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Navbar } from "./components/pages/Navbar";
+import { Navbar } from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
